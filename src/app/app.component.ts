6ae1151f48e7bc8f46adb8f661aeb42971c1c4bb
Jeison <jeison.hernandez-g@uniminuto.edu.co>
2023-05-18 20:34:00 -0500

    Some additional modules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Learning Angular';
-  description = 'Hola Osita Hermosa';
+  description = 'Hola Osithrfta Hermosa';
 }
